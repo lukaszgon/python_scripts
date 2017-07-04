a939ec4c281e21f3dc9e7301f7a3bcb11a5b9e46
lukaszgon <lukaszgon@hotmail.com>
2017-07-04 20:58:01 +0200

    printing number of results added

--- search_for.py
+++ search_for.py
@@ -9,6 +9,7 @@ import argparse
 # default test strings
 search_string = 'import'
 ending = '.py'
+results_num = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', action='store', dest='search_string')
@@ -28,4 +29,9 @@ for elem in dir_list:
         file_data = curr_file.read()
         if search_string in file_data:
             print('{} found in:   '.format(search_string), file_name, '\n')
+            results_num += 1
         curr_file.close()
+if results_num is 1:
+    print(results_num, 'result was found')
+else:
+    print(results_num, 'results were found')
